32986a22b84ed6df388065e8f590d9322b49e76c
Taro Kaneko <tetsuwan28go@gmail.com>
2013-10-27 09:34:06 +0900

    test

---
+++ HelloJni/obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/string.h \
+ C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/android/api-level.h \
+ C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/malloc.h \
+ C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/jni.h
+
+C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/string.h:
+
+C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/android/api-level.h:
+
+C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+C:/android-ndk-r9/platforms/android-5/arch-arm/usr/include/jni.h:
